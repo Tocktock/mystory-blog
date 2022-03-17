b98bf91f003f5985f6559354dbad6628b0041fea
tars <qnfmtm666@naver.com>
2022-03-17 14:21:08 +0900

    robot.txt -> robots.txt

--- scripts/generate-robot.js
+++ scripts/generate-robot.js
@@ -3,4 +3,4 @@ const fs = require('fs')
 const robotTXT =
   'User-agent: Googlebot-news\n' + 'Allow: / \n\n' + 'User-agent: *\n' + 'Disallow: / \n'
 
-fs.writeFileSync('public/robot.txt', robotTXT)
+fs.writeFileSync('public/robots.txt', robotTXT)
