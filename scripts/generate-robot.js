5f0c15a3df38866f98a010b05b5bf5bf9c2db222
tars <qnfmtm666@naver.com>
2022-03-17 14:24:49 +0900

    robot.txt  에sitemap 가에

--- scripts/generate-robot.js
+++ scripts/generate-robot.js
@@ -1,6 +1,10 @@
 const fs = require('fs')
 
 const robotTXT =
-  'User-agent: Googlebot-news\n' + 'Allow: / \n\n' + 'User-agent: *\n' + 'Disallow: / \n'
+  'User-agent: Googlebot-news\n' +
+  'Allow: / \n\n' +
+  'User-agent: *\n' +
+  'Disallow: / \n\n ' +
+  'Sitemap: https://mystory-blog.vercel.app/sitemap.xml \n'
 
 fs.writeFileSync('public/robots.txt', robotTXT)
