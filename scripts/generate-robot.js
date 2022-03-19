a53daf1a4dd664080de3c002d041c529dc4bc035
tars <qnfmtm666@naver.com>
2022-03-19 16:59:33 +0900

    naver agent add

--- scripts/generate-robot.js
+++ scripts/generate-robot.js
@@ -3,6 +3,8 @@ const fs = require('fs')
 const robotTXT =
   'User-agent: Googlebot\n' +
   'Allow: / \n\n' +
+  'User-agent: Yeti\n' +
+  'Allow:/ \n\n' +
   'User-agent: *\n' +
   'Disallow: / \n\n' +
   'Sitemap: https://mystory-blog.vercel.app/sitemap.xml \n'
