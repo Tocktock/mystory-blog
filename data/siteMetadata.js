e5d60aee426e8f9b1acd7ab9f51ed44ea55462f9
Tocktock <qnfmtm666@naver.com>
2024-02-05 22:30:33 +0900

    로드맵 수정, 뉴스레터 삭제

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -23,11 +23,7 @@ const siteMetadata = {
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     googleAnalyticsId: '178749172', // e.g. UA-000000-2 or G-XXXXXXX
   },
-  newsletter: {
-    // supports mailchimp, buttondown, convertkit, klaviyo
-    // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
-  },
+  newsletter: '',
   comment: {
     // Select a provider and use the environment variables associated to it
     // https://vercel.com/docs/environment-variables
