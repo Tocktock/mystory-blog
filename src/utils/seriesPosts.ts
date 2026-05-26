ca6bb96f9157615d33eab0527c47b40340e123e2
Park Ji Yong <qnfmtm666@naver.com>
2026-05-26 20:19:21 +0900

    Refine blog post styling and series ordering

--- src/utils/seriesPosts.ts
+++ src/utils/seriesPosts.ts
@@ -1,12 +1,22 @@
 import type { CollectionEntry } from 'astro:content';
 
 export const sortSeriesPosts = (a: CollectionEntry<'blog'>, b: CollectionEntry<'blog'>) => {
-	const aOrder = a.data.seriesOrder ?? Number.MAX_SAFE_INTEGER;
-	const bOrder = b.data.seriesOrder ?? Number.MAX_SAFE_INTEGER;
+  const aOrder = a.data.seriesOrder ?? Number.MAX_SAFE_INTEGER;
+  const bOrder = b.data.seriesOrder ?? Number.MAX_SAFE_INTEGER;
 
-	if (aOrder !== bOrder) {
-		return aOrder - bOrder;
-	}
+  if (aOrder !== bOrder) {
+    return aOrder - bOrder;
+  }
 
-	return a.data.pubDate.valueOf() - b.data.pubDate.valueOf();
+  return a.data.pubDate.valueOf() - b.data.pubDate.valueOf();
+};
+
+export const sortSeriesPostsByLatest = (a: CollectionEntry<'blog'>, b: CollectionEntry<'blog'>) => {
+  const dateDiff = b.data.pubDate.valueOf() - a.data.pubDate.valueOf();
+
+  if (dateDiff !== 0) {
+    return dateDiff;
+  }
+
+  return sortSeriesPosts(a, b);
 };
